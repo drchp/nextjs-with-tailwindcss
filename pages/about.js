6530939a69f13b42fdacbe46ad8106d49585d5dd
drchp <chipwiginton@me.com>
2021-01-14 13:30:35 -0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,4 +2,15 @@ function About() {
   return <div>About</div>
 }
 
-export default About
+export default About {
+  return (
+    <div>
+      <Nav />
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+          This is the about page
+        </h1>
+      </div>
+    </div>
+  )
+}
