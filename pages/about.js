a31a17dc112a963bc9f5bb265619bb5be9651b12
drchp <chipwiginton@me.com>
2021-01-14 13:54:39 -0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,14 @@
 import Nav from '../components/nav'
 
-function About() {
-  return <div>About</div>
-  
-  <div>
+export default function About() {
+  return (
+    <div>
       <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          This is the about page
+          This is the about page.
         </h1>
       </div>
     </div>
+  )
 }
